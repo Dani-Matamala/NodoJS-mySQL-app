00e88b94bde96f11a561371532229cae0724299f
Matamala Daniel <daniel20mat@gmail.com>
2021-08-15 15:08:22 -0300

    version estable, correccion de error

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 //initializations
 const app = express();
 
 // settings
 app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars'),
+}))
+app.set('view engine', '.hbs');
 
 // Middleawares
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 // Gloval Variables
 
 // Routes
+app.use(require('./routes'));
 
 // Public
 
