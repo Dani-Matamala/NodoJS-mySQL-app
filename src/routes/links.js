35ed8a2bbb02ca96e4091ab4cfa9577e453ee87f
Matamala Daniel <daniel20mat@gmail.com>
2021-08-15 23:29:16 -0300

    se cargaron algunas rutas y disenio la pagina add con boostrap

--- src/routes/links.js
+++ src/routes/links.js
@@ -3,4 +3,12 @@ const router = express.Router();
 
 const pool = require('../database');
 
+router.get('/add', function (req, res) {
+    res.render('links/add')
+})
+
+router.post('/add', function (req, res) {
+    res.send('recibido');
+});
+
 module.exports = router;
