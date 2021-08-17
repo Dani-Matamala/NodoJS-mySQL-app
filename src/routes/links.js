08e93700176f57f52700bcd03dd9096e07bf5af6
Matamala Daniel <daniel20mat@gmail.com>
2021-08-16 21:55:06 -0300

    configuracion para eliminar en nuestra base de datos

--- src/routes/links.js
+++ src/routes/links.js
@@ -6,7 +6,7 @@ const pool = require('../database');
 router.get('/', async (req, res) => {
     const links = await pool.query('SELECT * FROM links');
     console.log(links);
-    res.render('aqui iran los links');
+    res.render('links/list', {links});
 })
 
 router.get('/add', (req, res) => {
@@ -23,7 +23,13 @@ router.post('/add', async (req, res) => {
     };
     console.log(newLink);
     await pool.query('INSERT INTO links set ?', [newLink]);
-    res.send('recibido');
+    res.redirect('/links')
 });
+
+router.get('/delete/:id', async (req, res) => {
+    const {id} = req.params;
+    await pool.query('DELETE FROM links WHERE id = ?', [id]);
+    res.redirect('/links');
+})
 
 module.exports = router;
