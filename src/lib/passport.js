54f2816b778ed4dfdebd734bb99183ffc2d4b3e3
Matamala Daniel <daniel20mat@gmail.com>
2021-08-29 21:28:23 -0300

    seccion de singUp y register terminada

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -18,5 +18,15 @@ passport.use('local.signup', new LocalStrategy({
     };
     newUser.password = await helpers.encryptPassword(password);
     const result = await pool.query('INSERT INTO users SET ?', [newUser]);
-    console.log(result);
+    newUser.id = result.insertId;
+    return done(null, newUser);
 }));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+}); 
+
+passport.deserializeUser(async(id, done) => {
+    const rows = await pool.query('SELECT * FROM users WHERE id = ?', [id]);
+    done(null, rows[0]);
+})
